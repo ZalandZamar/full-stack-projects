3fad86bf2a48200103cd9eb69c0933c8973d9c26
Zaland <sanikhankhan749@gmail.com>
2025-12-06 19:53:22 -0800

    can now get a single task

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -16,8 +16,18 @@ const createTasks = async (req, res) => {
     res.status(500).json({ message: error });
   }
 };
-const getSingleTask = (req, res) => {
-  res.send("single task!");
+const getSingleTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `did not find the id ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 const updateTasks = (req, res) => {
   res.send("update task!");
