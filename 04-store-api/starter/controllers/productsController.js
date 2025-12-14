708d3de3dd3566c1d3c8a5176e83941d5ed7697a
Zaland <sanikhankhan749@gmail.com>
2025-12-14 15:52:07 -0800

    can now sort and select based on property name

--- 04-store-api/starter/controllers/productsController.js
+++ 04-store-api/starter/controllers/productsController.js
@@ -1,12 +1,12 @@
 const product = require("../models/productSchema");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({ name: "vase table" });
+  const products = await product.find({}).select("name price createdAt");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -17,7 +17,22 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
 
-  const products = await product.find(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let result = product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
